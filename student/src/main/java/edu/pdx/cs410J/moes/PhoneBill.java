f4a3ce756c26cab72cf879263fd8ed550942f4ba
NathanMoes <91927252+NathanMoes@users.noreply.github.com>
2022-06-25 12:49:26 -0700

    Basic class

--- student/src/main/java/edu/pdx/cs410J/moes/PhoneBill.java
+++ student/src/main/java/edu/pdx/cs410J/moes/PhoneBill.java
@@ -1,5 +1,29 @@
 package edu.pdx.cs410J.moes;
 
-public class PhoneBill {
+import edu.pdx.cs410J.AbstractPhoneBill;
+import edu.pdx.cs410J.AbstractPhoneCall;
+import java.util.ArrayList;
+
+import java.util.Collection;
+
+public class PhoneBill extends  AbstractPhoneBill{
+
+    public String CustomerName;
+    public ArrayList<PhoneCall> calls;
+
+    @Override
+    public String getCustomer() {
+        return null;
+    }
+
+    @Override
+    public void addPhoneCall(AbstractPhoneCall abstractPhoneCall) {
+
+    }
+
+    @Override
+    public Collection getPhoneCalls() {
+        return null;
+    }
 
 }
