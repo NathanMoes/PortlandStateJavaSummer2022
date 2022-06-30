f6c263e4e356b299ea2d7c30c0b9b95c21c2e5ab
NathanMoes <nathanmoes7@gmail.com>
2022-06-30 12:47:21 -0700

    Added basic stuff

--- student/src/main/java/edu/pdx/cs410J/moes/Project1.java
+++ student/src/main/java/edu/pdx/cs410J/moes/Project1.java
@@ -2,9 +2,10 @@ package edu.pdx.cs410J.moes;
 
 public class Project1 {
     public static void main(String[] args) {
-        if (args.length < 4){
+        if (args.length <= 1){
             System.out.println("not enough arguments");
             System.out.println(args.length);
         }
+        PhoneCall temp = new PhoneCall(args[0], args[1], args[2], args[3], args[4], args[5]);
     }
 }
