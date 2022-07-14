392f06111574e09260ab7500b19a03fc89a26ac7
NathanMoes <nathanmoes7@gmail.com>
2022-07-13 18:53:11 -0700

    koans

--- koans/src/beginner/AboutPrimitives.java
+++ koans/src/beginner/AboutPrimitives.java
@@ -45,8 +45,8 @@ public class AboutPrimitives {
 
     @Koan
     public void longsHaveALargerRangeThanInts() {
-        assertEquals(Long.MIN_VALUE, -2147483648);
-        assertEquals(Long.MAX_VALUE, 2147483647);
+        assertEquals(Long.MIN_VALUE, -9223372036854775808);
+        assertEquals(Long.MAX_VALUE, 9223372036854775807);
     }
 
     @Koan
