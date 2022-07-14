3203de303b7763be566d79cf53447fd4d7176e8b
NathanMoes <nathanmoes7@gmail.com>
2022-07-13 19:58:42 -0700

    koans

--- koans/src/beginner/AboutCasting.java
+++ koans/src/beginner/AboutCasting.java
@@ -91,7 +91,7 @@ public class AboutCasting {
         Parent parent = (Child) child;
         // Think about the result. Did you expect that? Why?
         // How is that different from above?
-        assertEquals(parent.complain(), __);
+        assertEquals(parent.complain(), "Are we there yet!!");
     }
 
     @Koan
@@ -108,7 +108,7 @@ public class AboutCasting {
     public void complicatedCast() {
         Grandparent parent = new Parent();
         // How can we access the parent's ability to "complain" - if the reference is held as a superclass?
-        assertEquals("TPS reports don't even have a cover letter!", __);
+        assertEquals("TPS reports don't even have a cover letter!", parent.sleep());
     }
 
 }
