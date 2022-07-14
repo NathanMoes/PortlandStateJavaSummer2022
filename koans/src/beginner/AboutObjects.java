dfbd5364b75a15b4fc9f79084d12b9a896ba1cec
NathanMoes <nathanmoes7@gmail.com>
2022-07-13 19:29:09 -0700

    koans

--- koans/src/beginner/AboutObjects.java
+++ koans/src/beginner/AboutObjects.java
@@ -13,7 +13,7 @@ public class AboutObjects {
 
     @Koan
     public void newObjectInstancesCanBeCreatedDirectly() {
-        assertEquals(new Object() instanceof Object, __);
+        assertEquals(new Object() instanceof Object, Object.class);
     }
 
     @Koan
@@ -22,8 +22,8 @@ public class AboutObjects {
         }
 
         Class<?>[] ancestors = getAncestors(new Foo());
-        assertEquals(ancestors[0], __);
-        assertEquals(ancestors[1], __);
+        assertEquals(ancestors[0], 1);
+        assertEquals(ancestors[1], 1);
     }
 
     @Koan
